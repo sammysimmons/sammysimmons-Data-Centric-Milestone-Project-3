a2692d527204af0b0690474bfb8b3e0d27cce745
Samantha Simmons <sammysimmons274@gmail.com>
2020-07-12 21:05:35 +0100

    recipes updated name changed

--- run.py
+++ run.py
@@ -4,9 +4,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/recipe")
+def recipe():
+    return render_template("recipe.html")
 
 if __name__ == '__main__':
            app.run(host=os.environ.get('IP', '127.0.0.1'),              
